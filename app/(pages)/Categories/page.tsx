7fd6b323148ea3fb6d6e0aea6f0c51466e7dee2f
N1ewb <nathaniellucero20@gmail.com>
2024-08-16 23:54:41 +0800

    123123

--- app/(pages)/Categories/page.tsx
+++ app/(pages)/Categories/page.tsx
@@ -3,7 +3,7 @@ import { showList } from "@/lib/global";
 import Cards from "@/app/(components)/Cards/page";
 import { Category } from "@/lib/types";
 
-const Categories: React.FC = () => {
+const Categories = () => {
   return (
     <div className="categories-container relative top-[210px] text-white w-full p-10 bg-black">
       <h1 className="font-bold text-[35px]">Categories</h1>
