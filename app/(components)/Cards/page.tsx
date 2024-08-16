731f858882841a71271a13856b0dc155d6ffea4f
N1ewb <nathaniellucero20@gmail.com>
2024-08-17 00:01:30 +0800

    123123

--- app/(components)/Cards/page.tsx
+++ app/(components)/Cards/page.tsx
@@ -17,17 +17,59 @@ const Comedy = filterShowsByCategory(showList, "Comedy");
 
 const Cards = () => {
   return (
-    <div className="flex flex-row gap-5 p-10 max-w-full overflow-auto scrollbar-hide">
-      {Romcom && Romcom.length > 0 ? (
-        Romcom.map((show, index) => (
-          <div key={index} className="flex-shrink-0 w-[160px] text-center">
-            <Image src={show.src} alt={show.name} height={160} width={160} />
-            <p>{show.name}</p>
-          </div>
-        ))
-      ) : (
-        <p>No movies available</p>
-      )}
+    <div className="flex flex-col ">
+      <h1>Rom Com</h1>
+      <div className="romcom-container flex flex-row gap-5 p-10 max-w-full overflow-auto scrollbar-hide">
+        {Romcom && Romcom.length > 0 ? (
+          Romcom.map((show, index) => (
+            <div key={index} className="flex-shrink-0 w-[160px] text-center">
+              <Image src={show.src} alt={show.name} height={160} width={160} />
+              <p>{show.name}</p>
+            </div>
+          ))
+        ) : (
+          <p>No Rom Com Movies available</p>
+        )}
+      </div>
+      <h1>Drama</h1>
+      <div className="drama-container flex flex-row gap-5 p-10 max-w-full overflow-auto scrollbar-hide">
+        {Drama && Drama.length > 0 ? (
+          Drama.map((show, index) => (
+            <div key={index} className="flex-shrink-0 w-[160px] text-center">
+              <Image src={show.src} alt={show.name} height={160} width={160} />
+              <p>{show.name}</p>
+            </div>
+          ))
+        ) : (
+          <p>No Drama Movies available</p>
+        )}
+      </div>
+      <h1>Action</h1>
+      <div className="action-container flex flex-row gap-5 p-10 max-w-full overflow-auto scrollbar-hide">
+        {Action && Action.length > 0 ? (
+          Action.map((show, index) => (
+            <div key={index} className="flex-shrink-0 w-[160px] text-center">
+              <Image src={show.src} alt={show.name} height={160} width={160} />
+              <p>{show.name}</p>
+            </div>
+          ))
+        ) : (
+          <p>No Action Movies available</p>
+        )}
+      </div>
+      <h1>Comedy</h1>
+      <div className="comedy-container flex flex-row gap-5 p-10 max-w-full overflow-auto scrollbar-hide">
+        {Comedy && Comedy.length > 0 ? (
+          Comedy.map((show, index) => (
+            <div key={index} className="flex-shrink-0 w-[160px] text-center">
+              <Image src={show.src} alt={show.name} height={160} width={160} />
+              <p>{show.name}</p>
+            </div>
+          ))
+        ) : (
+          <p>No Comedy Movies available</p>
+        )}
+      </div>
     </div>
   );
 };
--- app/(pages)/Categories/page.tsx
+++ app/(pages)/Categories/page.tsx
@@ -7,22 +7,8 @@ const Categories = () => {
   return (
     <div className="categories-container relative top-[210px] text-white w-full p-10 bg-black">
       <h1 className="font-bold text-[35px]">Categories</h1>
-      <div>
-        <h3>Rom Com</h3>
-        <Cards />
-      </div>
-      <div>
-        <h3>Drama</h3>
-        <Cards />
-      </div>
-      <div>
-        <h3>Action</h3>
-        <Cards />
-      </div>
-      <div>
-        <h3>Comedy</h3>
-        <Cards />
-      </div>
+
+      <Cards />
     </div>
   );
 };
