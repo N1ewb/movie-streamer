ca80c9302c6699986cc9cc81d140f193380cecb5
N1ewb <nathaniellucero20@gmail.com>
2024-08-30 01:03:35 +0800

    asd

--- app/(pages)/SearchPage/page.tsx
+++ app/(pages)/SearchPage/page.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import PlayMovieContent from "@/components/PlayMovieContent";
+
 import SearchPage from "../Search/page";
 
 const SearchMoviePage = () => {
